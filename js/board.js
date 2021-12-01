8be22778f2bf58821319dc52785d61df4cbf94f5
ckstmznf <ckstmznf@naver.com>
2021-12-02 08:31:58 +0900

    게시물 작성할때 이동하는 페이지 지정

--- js/board.js
+++ js/board.js
@@ -10,7 +10,8 @@ function getUrlParams() {
     return params;
 }
 
-const id = getUrlParams()["id"] * 1
+const querys = getUrlParams()
+const id = querys["id"] * 1
 
 //matching
 let title = document.querySelector(".title")
@@ -21,21 +22,32 @@ let content = document.querySelector(".content")
 let prevBtn = document.querySelector(".buttons .prev")
 let nextBtn = document.querySelector(".buttons .next")
 
-if(id == 0){
+if(isNaN(id)){
+    console.log("Hello World")
+    title.innerText = querys["title"]
+    writer.innerText = "박희찬"
+    viewCount.innerText = 0
+    content.innerText = querys["content"]
+}else if(id == 0){
     prevBtn.hidden = true;
+    setupBoard()
 }   else if(id == data.length - 1){
     nextBtn.hidden = true;
+    setupBoard()
 }
 
-prevBtn.addEventListener("click", () => {
-    location.href = `./board.html?id=${id - 1}`
-})
+function setupBoard(){
+    prevBtn.addEventListener("click", () => {
+        location.href = `./board.html?id=${id - 1}`
+    })
+    
+    nextBtn.addEventListener("click", () => {
+        location.href = `./board.html?id=${id + 1}`
+    })
+    
+    title.innerText = data[id].title
+    writer.innerText = data[id].writer
+    viewCount.innerText = data[id].viewCount
+    content.innerText = data[id].content
+}
 
-nextBtn.addEventListener("click", () => {
-    location.href = `./board.html?id=${id + 1}`
-})
-
-title.innerText = data[id].title
-writer.innerText = data[id].writer
-viewCount.innerText = data[id].viewCount
-content.innerText = data[id].content
